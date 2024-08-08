d2a85512eee3b1cf24355270a5f3117be7e6bc01
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-08 20:14:44 +0530

    services: Add freeform system service
    neobuddy89: Create package to include in fwb base itself.
    
    Change-Id: Icb3933846fc38f7fe1679e7f1fd916c2c137671d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -133,6 +133,7 @@ import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.display.DcDimmingService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.DisplayManagerService;
+import com.android.server.display.FreeformService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.emergency.EmergencyAffordanceService;
@@ -2593,6 +2594,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartAuthService");
             mSystemServiceManager.startService(AuthService.class);
+            t.traceEnd();
+
+            t.traceBegin("FreeformService");
+            mSystemServiceManager.startService(FreeformService.class);
             t.traceEnd();
 
             if (!isWatch) {
