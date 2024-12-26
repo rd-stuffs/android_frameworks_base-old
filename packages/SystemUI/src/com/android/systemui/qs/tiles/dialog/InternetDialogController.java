35e18c978e35991ee00aa8d09db14b0de1bca5c8
Richard Raya <rdxzv.dev@gmail.com>
2024-12-26 17:07:14 -0300

    Revert "SystemUI: Turn on WiFi when pressing the Internet tile"
    This reverts commit 4d780b14857d71d2dbd1728c8d4d8c3326c85dc8.
    
    Change-Id: Ia4e56a20a82464ea56da91cd152a39c34e627c05
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogController.java
@@ -411,9 +411,6 @@ public class InternetDialogController implements AccessPointController.AccessPoi
         } else {
             notifyDualDataEnabledStateChanged();
         }
-
-        if (!isWifiEnabled())
-            setWifiEnabled(true);
     }
 
     void onStop() {
